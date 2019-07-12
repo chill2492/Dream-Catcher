2a1e36b7dc94138775df067ab43929ae1693eb78
chill2492 <38290324+chill2492@users.noreply.github.com>
2019-07-12 11:38:45 -0400

    Add files via upload

---
+++ generated/source/buildConfig/androidTest/debug/edu/vt/cs/cs5254/dreamcatcher/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.vt.cs.cs5254.dreamcatcher.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.vt.cs.cs5254.dreamcatcher.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
